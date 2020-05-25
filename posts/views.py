69078165c90bebb7cdc2eaa094c25db86b2abd76
Krzysztof Suder <ksuder7@gmail.com>
2020-05-25 14:08:13 +0200

    create templates

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("<h1>Home Page</h1>")
+    return render(request, 'posts/home.html')
 
 
 def about(request):
-    return HttpResponse("<h1>About Page</h1>")
+    return render(request, 'posts/about.html')
