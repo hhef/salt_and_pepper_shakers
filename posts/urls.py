4feb4a32b7c467125a7bb20802671b56e3218a43
Krzysztof Suder <ksuder7@gmail.com>
2020-05-26 16:57:49 +0200

    add urls in base.html

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home-page"),
     path("about/", views.about, name="about-page"),
-    path("contact/", views.contact, name="about-page"),
+    path("contact/", views.contact, name="contact-page"),
 ]
