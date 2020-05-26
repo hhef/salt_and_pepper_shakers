a0da41fbe9731e5b836830f795418ec7f577970a
Krzysztof Suder <ksuder7@gmail.com>
2020-05-26 18:26:53 +0200

    create models and home page

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def home(request):
-    return render(request, 'posts/home.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'posts/home.html', context)
 
 
 def about(request):
