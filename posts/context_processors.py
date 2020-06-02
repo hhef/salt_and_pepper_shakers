972eb2d1d4489c223fd67a26bee1b598b6361c21
Krzysztof Suder <ksuder7@gmail.com>
2020-06-02 20:39:37 +0200

    add categories to base.html

--- posts/context_processors.py
+++ posts/context_processors.py
@@ -1,7 +1,12 @@
-from posts.models import Post
+from posts.models import Post, Category
 from random import sample
 
 
 def slider(request):
     random_posts = Post.objects.filter(name='Elvis')
     return {'random_posts': random_posts}
+
+
+def categories(request):
+    categories = Category.objects.all()
+    return {'categories': categories}
