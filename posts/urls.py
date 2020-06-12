475def9e01114c56366091e49f7261bdaba10134
Krzysztof Suder <ksuder7@gmail.com>
2020-06-12 19:05:50 +0200

    change home page view to class based view

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import home, about, contact, SearchListView
+from .views import about, contact, SearchListView, PostListView
 
 urlpatterns = [
-    path("", home, name="home-page"),
+    path("", PostListView.as_view(), name="home-page"),
     path("about/", about, name="about-page"),
     path("contact/", contact, name="contact-page"),
     path('search/', SearchListView.as_view(), name='search')
--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
 from django.db.models import Q
-
-
-def home(request):
-    context = {
-        'posts': Post.objects.all().order_by('-date_posted')
-    }
-    return render(request, 'posts/home.html', context)
+from django.views.generic import ListView
 
 
 def about(request):
@@ -32,3 +25,10 @@ class SearchListView(ListView):
             Q(author__username__icontains=query)
         ).distinct()
         return queryset_list.order_by('-date_posted')
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'posts/home.html'
+    context_object_name = 'posts'
+    ordering = '-date_posted'
